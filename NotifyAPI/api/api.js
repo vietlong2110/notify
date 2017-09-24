8b12d3261be0c3e48d8d8e9e3b985a955d44c245
Hai Cao <vietlong2110@gmail.com>
2017-09-24 19:20:34 +0700

    standard json rest api

--- NotifyAPI/api/api.js
+++ NotifyAPI/api/api.js
@@ -1,3 +1,27 @@
 /*******************************************************************************
 *                     WRITE API ROUTER IN THIS FILE                            *
 *******************************************************************************/
+
+const express = require("express");
+const router = express.Router();
+
+//Controller and Model to use
+const Controllers = require('../controllers');
+const Models = require('../database');
+
+/**
+* Search API
+* @param {req} REQUEST QUERY FORMAT: ?q=...&size=
+* @param {res} RESPONSE
+* JSON FORMAT:
+{
+  success: Bool,
+  payload: Array<Object> || null,
+  error: Array<Object> || null
+}
+*/
+router.get('/search', (req, res) => {
+
+});
+
+module.exports = router;
