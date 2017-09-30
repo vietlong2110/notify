6d59441f48e1582109f47377fceb898eba2cb425
Hai Cao <vietlong2110@gmail.com>
2017-09-30 09:21:43 +0700

    setup docker

--- NotifyAPI/indexing.js
+++ NotifyAPI/indexing.js
@@ -1,13 +1,53 @@
 /*******************************************************************************
 *           INDEXING EVERY ARTICLES OF THE APP HERE (NEWS OR SOCIAL)           *
 *******************************************************************************/
+// const Promise = require('bluebird');
+// const fs = Promise.promisifyAll(require('fs'));
 
 const { fetchNews } = require('./scraping/news');
 const { Sources } = require('./database');
 
 let cache = [];
 
+// const printSources = async() => {
+//   let sources = await Sources.find().exec();
+//   let writeArr = [];
+//   for (i = 0; i < sources.length; i++)
+//     writeArr = writeArr.concat({
+//       source: sources[i].source,
+//       links: sources[i].links
+//     });
+//   fs.writeFileAsync('./seeds/source.json', JSON.stringify(writeArr), 'utf-8')
+//   .then(() => {
+//     console.log('Initialize seeds successfully');
+//   }).catch(err => {
+//     console.log(err);
+//   })
+// };
+
+const initialize = async() => {
+  const sourceSeed = require('./seeds/source.json');
+  console.log('Checking exist seed data for indexing...');
+  for (i in sourceSeed) {
+    try {
+      let s = await Sources.findOne({ source: sourceSeed[i].source }).exec();
+      if (!s) {
+        let source = new Sources({
+          source: sourceSeed[i].source,
+          links: sourceSeed[i].links
+        });
+        await source.save();
+        console.log(i + 'Imported ' + sourceSeed[i].source + ' to seed data');
+      }
+    } catch(err) {
+      console.log(err);
+    }
+  }
+};
+
 const indexNews = async() => {
+  await initialize();
+
   while (1) {
     try {
       cache = await Sources.find().exec();
@@ -31,4 +71,6 @@ const indexNews = async() => {
   }
 };
 
+// initialize();
+// printSources();
 indexNews();
