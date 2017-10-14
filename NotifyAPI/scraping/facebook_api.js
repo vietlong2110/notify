935e085f1e9ced6e734b01bf6cb2dc5b7c9a2640
dinhthangbkhn <dinhthangbkhn@gmail.com>
2017-10-14 17:16:19 +0700

    get facebook and save to mongo, todo: set time out

--- NotifyAPI/scraping/facebook_api.js
+++ NotifyAPI/scraping/facebook_api.js
@@ -9,6 +9,7 @@ const TEST_TOKEN = 'EAAM98EFnHGMBAGxaiKBH98ZAvXkrQaoZADzayhKjQGrTuS4FV55yKaL1NoZ
 const TEST_PAGEID = '140284136008345';
 const DEFAULT_LANG = 'vi';
 const DEFAULT_TIMEOUT = 10000;
+const { Articles } = require('../database');
 
 const getUserLikePage = async (access_token = TEST_TOKEN) => {
     try {
@@ -54,12 +55,12 @@ const getFanPagePosts = async (page_id = '140284136008345', access_token = TEST_
         if (response.error)
             return Promise.reject(response.error);
         let data = response.data;
-        while (response.paging && response.paging.next) {
+        while (response.paging && response.paging.cursors.after) {
             try {
                 response = await fb.api(page_id + '/posts/' + response.paging.cursors.next, {
                     access_token,
                     limit: LIMIT,
-                    fields: 'created_time,story,full_picture,message'
+                    fields: 'created_time,story,message,full_picture,type,description,link,attachments'
                 })
                 if (response.error)
                     return Promise.resolve(data);
@@ -75,8 +76,75 @@ const getFanPagePosts = async (page_id = '140284136008345', access_token = TEST_
     }
 }
 
+const extractContent = async (link, created_time, id, message, story, full_picture) => {
+    try {
+        // let link = link ;
+        let publishedDate = created_time;
+        let source = id ;
+        let content = message ;
+        let title = "title" ;
+        let image = full_picture ;
+        // let tags = '';
+        let description = 'facebook';
+        let data = {
+            link: link, publishedDate: publishedDate, source: source,
+            content: content,
+            title: title ,
+            image: image,
+            // tags: tags,
+            description: description 
+        };
+        // console.log(data);
+        console.log('Extracted ' + link);
+        return Promise.resolve(data);
+    } catch (err) {
+        console.log('ERROR OCCURED when EXTRACTING ');
+        return Promise.reject(err);
+    }
+};
+
+const saveArticle = async (data, language = DEFAULT_LANG) => {
+    let { link, publishedDate, content, title, image, source, description } = data;
+    try {
+        let newArticle = new Articles({
+            link, publishedDate, content, title, image, source, description
+        });
+        await newArticle.save();
+        console.log('Indexed ' + link);
+        return Promise.resolve();
+    } catch (err) {
+        console.log('ERROR OCCURED when INDEXING ' + link);
+        return Promise.reject(err);
+    }
+};
 // getUserLikePage().then(value => {
 //     console.log(value);
 // })
 
-getFanPagePosts().then( value => console.log(value));
+const fetchFbFanpage = async (access_token = TEST_TOKEN, language = DEFAULT_LANG) => {
+    let user_liked_pages = await getUserLikePage(access_token);
+    console.log(user_liked_pages);
+    for (let i=0; i< user_liked_pages.length; i++) {
+        console.log("fetching "+user_liked_pages[i]);
+        try {
+            let page_posts = await getFanPagePosts(user_liked_pages[i].id, access_token);
+            console.log(page_posts);
+            for (let j=0; j<page_posts.length; j++) {
+                try {
+                    let {link, created_time, id, message, story, full_picture} = page_posts[j]; 
+                    let post_data = await extractContent(link, created_time, id, message, story, full_picture);
+                    await saveArticle(post_data, language);
+                } catch (err) {
+                    console.log(err);
+                }
+            }
+
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+}
+
+fetchFbFanpage();
+// getFanPagePosts().then(value => console.log(value));
