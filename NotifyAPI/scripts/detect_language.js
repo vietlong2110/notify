4d50c4a3ad974344d99320ec02969481c093f8aa
dinhthangbkhn <dinhthangbkhn@gmail.com>
2018-02-18 08:59:45 +0700

    finish:

--- NotifyAPI/scripts/detect_language.js
+++ NotifyAPI/scripts/detect_language.js
@@ -1,46 +1,112 @@
 const Models = require('../database');
 const LanguageDetect = require('languagedetect');
 const lngDetector = new LanguageDetect();
+const DEFAULT_INDEX = "articles";
+const DEFAULT_TYPE = "articles";
+
+const removeSymbols = body => {
+    let s = body.toString();
+    s = s.replace(/[\n\r]+/g, ' ');
+    s = s.replace(/[\*\^\+\?\\\.\[\]\^\$\|\{\)\(\}\'\"\~!\/@£$%&=`´“”‘’;><:,]+/g, '');
+    s = s.replace(/\s\s+/g, ' ');
+    // console.log(s);
+    return s;
+};
+
 const detect_language = (title) => {
-    // console.log(title)
+    //return
+    //1: eng
+    //0: vie
+    //-1: undefined
+
     result = lngDetector.detect(title)
-    // console.log(result)
     if (result.length != 0) {
-        // console.log(1)
-        // console.log(result)
         for (var i = 0; i < result.length; i++) {
             language_with_prob = result[i]
             if (language_with_prob[0] == 'vietnamese') {
-                return 1
-            }
-            if ( language_with_prob[0] == 'english') {
                 return 0
+            }
+            if (language_with_prob[0] == 'english') {
+                return 1
             }
         }
         return -1
     }
-    // console.log(2)
-    // vie_score = result
 }
 
-// console.log(detect_language(""))
-const create__documents = async () => {
+const create_documents = async (index = DEFAULT_INDEX, type = DEFAULT_TYPE) => {
+    //return body: an array of articles
     let k = 0
-    let total = await Models.Articles.count({}) 
+    let total = await Models.Articles.count({})
     console.log(total)
-    let body = []   
+    let body = []
     while (k < total) {
         let articles = await Models.Articles.find({}).limit(3000).skip(k).exec();
-        for ( let i = 0; i < articles.length; i++) {
-            if ( i > 6) {
-                
-                return 
+        k += 3000
+        for (let i = 0; i < articles.length; i++) {
+            let description = removeSymbols(articles[i].description)
+            let title = removeSymbols(articles[i].title)
+            let content = removeSymbols(articles[i].content)
+            let article = {}
+            if (detect_language(title) == 1) {
+                article = {
+                    index: index,
+                    type: type,
+                    id: articles[i]._id.toString(),
+                    body_eng: {
+                        image: articles[i].image,
+                        link: articles[i].link,
+                        description, 
+                        title, 
+                        content,
+                        source: articles[i].source,
+                        publishedDate: articles[i].publishedDate,
+                        tags: articles[i].tags,
+                        video: articles[i].video
+                    },
+                    body_vi: {
+                        image: null,
+                        link: null,
+                        description: null,
+                        title: null,
+                        content: null,
+                        source: null,
+                        publishedDate: null,
+                        tags: null,
+                        video: null
+                    }
+                }
+            } else if (detect_language(title) == 0) {
+                article = {
+                    index: index,
+                    type: type,
+                    id: articles[i]._id.toString(),
+                    body_vi: {
+                        image: articles[i].image,
+                        link: articles[i].link,
+                        description, title, content,
+                        source: articles[i].source,
+                        publishedDate: articles[i].publishedDate,
+                        tags: articles[i].tags,
+                        video: articles[i].video
+                    },
+                    body_eng: {
+                        image: null,
+                        link: null,
+                        description: null,
+                        title: null,
+                        content: null,
+                        source: null,
+                        publishedDate: null,
+                        tags: null,
+                        video: null
+                    }
+                }
             }
-            // console.log(articles)
-            console.log(articles[i].title)
-            console.log(detect_language(articles[i].title))
+            body.push(article)
         }
+        console.log(k)
     }
+    return body
 }
-
-create_documents()
+create_documents().then( v => console.log("body" +v))
